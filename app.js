72307fc0718ac9821bb0f64c31bf963410989fa3
Kim Tran Do <Kim.Tran-Do@metropolia.fi>
2024-09-16 13:56:56 +0300

    Connected to Mongo Atlas

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
-const { unknownEndpoint } = require("./middleware/customMiddleware");
+const { unknownEndpoint, errorHandler } = require("./middleware/customMiddleware");
 const connectDB = require("./config/db");
 require('dotenv').config();
 
@@ -20,8 +20,15 @@ app.use("/api/tours", tourRouter);
 // Use the userRouter for all /users routes
 app.use("/api/users", userRouter);
 
+// Example route that throws an error
+app.get('/error', (req, res, next) => {
+  // Trigger an error
+  const error = new Error("Something went wrong!");
+  next(error);
+});
+
 app.use(unknownEndpoint);
-// app.use(errorHandler);
+app.use(errorHandler);
 
 const port = process.env.PORT || 4000;
 // Start the server
